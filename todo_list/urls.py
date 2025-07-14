ab76c5592de6f9e13322cd9d93ace2ea78fee517
Polevoy Yevhen <polevoye90@gmail.ua>
2025-07-14 12:38:36 +0300

    add debug toolbars

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("task.urls", namespace="task")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
