67519f78be54a3debbd42d0a9dc2d7b842143bab
Polevoy Yevhen <polevoye90@gmail.ua>
2025-07-14 13:20:56 +0300

    add forms.py / add TaskForm / add TaskCreateForm

--- task/views.py
+++ task/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
+from .forms import TaskForm
 from .models import Task, Tag
 
 
@@ -14,4 +16,11 @@ class TagListView(generic.ListView):
     model = Tag
     context_object_name = "tag_list"
     template_name = "todo/tag_list.html"
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = "todo/task_form.html"
+    success_url = reverse_lazy("task:task-list")
 
--- todo_list/settings.py
+++ todo_list/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "debug_toolbar",
+    "crispy_forms",
+    "crispy_bootstrap4",
     "task",
 ]
 
@@ -70,6 +72,8 @@ TEMPLATES = [
         },
     },
 ]
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 WSGI_APPLICATION = "todo_list.wsgi.application"
 
